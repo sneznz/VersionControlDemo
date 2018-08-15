6ccd73f964898a6dba664f4f792e657e08ae0b45
Snezana Mihic <mihics@redrock.net.au>
2018-08-15 15:47:27 +1200

    Initial commit

---
+++ app/src/main/java/com/example/versioncontroldemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.versioncontroldemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // testing version control
+
+        TextView test;
+
+        test = findViewById(R.id.helloWorldTextView);
+        test.setText("snez");
+
+    }
+}
